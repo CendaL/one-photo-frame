a36b91af96aa272bf3cf62cdaeba5de330e4aea0
Lukas Cenovsky <lucen@microsoft.com>
2018-08-06 23:18:54 +0200

    selecting folders

--- src/test.vue
+++ src/test.vue
@@ -3,7 +3,13 @@
     <button @click="load()">load</button>
     <ul>
       <li v-for="item in folders" :key="item.id">
-        <span @click="load(item)">{{ item.name }}</span>
+        <button @click="add(item)">+</button>
+        <button @click="load(item)">{{ item.name }}</button>
+      </li>
+    </ul>
+    <ul>
+      <li v-for="item in selected" :key="item.id">
+        <button @click="remove(item)">{{ item.name }} ({{item.id}})</button>
       </li>
     </ul>
   </div>
@@ -18,7 +24,8 @@ import Log from "./Log.vue";
 export default {
   data() {
     return {
-      folders: [{ id: 1, name: "def" }]
+      folders: [{ id: 1, name: "def" }],
+      selected: []
     };
   },
   components: {
@@ -36,6 +43,21 @@ export default {
       graphService.listPhotoFolders(baseFolder).then(data => {
         this.folders = data;
       });
+    },
+    add(baseFolder) {
+      console.log(`add ${JSON.stringify(baseFolder)}`);
+      if (
+        baseFolder &&
+        baseFolder.id &&
+        baseFolder.name !== ".." &&
+        this.selected.filter(i => i.id === baseFolder.id).length === 0
+      ) {
+        this.selected.push({ id: baseFolder.id, name: baseFolder.name || baseFolder.id });
+      }
+    },
+    remove(baseFolder) {
+      console.log(`remove ${JSON.stringify(baseFolder)}`);
+      this.selected = this.selected.filter(i => i.id !== baseFolder.id);
     }
   }
 };
@@ -43,7 +65,15 @@ export default {
 
 <style scoped>
 li {
-  color: white;
+  list-style-type: none;
+}
+button {
+  font-family: sans-serif;
+  color: gray;
+  background-color: transparent;
+  border-width: 0px;
+  text-align: left;
+  font-size: larger;
 }
 </style>
 
