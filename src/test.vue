d781087b7c524278298859120abbe99ddd90d14d
Lukas Cenovsky <lucen@microsoft.com>
2018-08-07 23:32:42 +0200

    adjust ui

--- src/test.vue
+++ src/test.vue
@@ -1,17 +1,22 @@
 <template>
   <div>
-    <button @click="load()">load</button>
-    <ul>
-      <li v-for="item in folders" :key="item.id">
-        <button @click="add(item)">+</button>
-        <button @click="load(item)">{{ item.name }}</button>
-      </li>
-    </ul>
-    <ul>
-      <li v-for="item in selected" :key="item.id">
-        <button @click="remove(item)">{{ item.name }} ({{item.id}})</button>
-      </li>
-    </ul>
+    <div class="photo-folders">
+      <h2>Přidat fotky:</h2>
+      <ul>
+        <li v-for="item in folders" :key="item.id">
+          <button @click="add(item)">+</button>
+          <button @click="load(item)">{{ item.name }}</button>
+        </li>
+      </ul>
+    </div>
+    <div class="selected-photos">
+      <h2>Vybrané fotky:</h2>
+      <ul>
+        <li v-for="item in selected" :key="item.id">
+          <button @click="remove(item)">{{ item.name }} ({{item.id}})</button>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -19,33 +24,31 @@
 import { mapActions, mapGetters, mapMutations, mapState } from "vuex";
 import { log, logError } from "./utils";
 import graphService from "./services/graph.service";
-import Log from "./Log.vue";
 
 export default {
   data() {
     return {
-      folders: [{ id: 1, name: "def" }],
+      folders: [{ name: "nahrávám..." }],
       selected: []
     };
-  },
-  components: {
-    log: Log
   },
   computed: {
     // ...mapState(["folders"]),
     ...mapGetters(["isSignedIn"])
   },
-  created() {},
+  created() {
+    this.load();
+  },
   methods: {
     ...mapActions([]),
-    ...mapMutations(["setFolders", "setStatusText"]),
+    ...mapMutations(["setFolders"]),
     load(baseFolder) {
       graphService.listPhotoFolders(baseFolder).then(data => {
         this.folders = data;
       });
     },
     add(baseFolder) {
-      console.log(`add ${JSON.stringify(baseFolder)}`);
+      log(`add ${JSON.stringify(baseFolder)}`);
       if (
         baseFolder &&
         baseFolder.id &&
@@ -56,7 +59,7 @@ export default {
       }
     },
     remove(baseFolder) {
-      console.log(`remove ${JSON.stringify(baseFolder)}`);
+      log(`remove ${JSON.stringify(baseFolder)}`);
       this.selected = this.selected.filter(i => i.id !== baseFolder.id);
     }
   }
@@ -64,14 +67,38 @@ export default {
 </script>
 
 <style scoped>
+div.selected-photos,
+div.photo-folders {
+  position: fixed;
+  width: 50%;
+  top: 0;
+  bottom: 0;
+  overflow-y: auto;
+  overflow-x: hidden;
+}
+div.selected-photos {
+  right: 0;
+}
+ul {
+  -webkit-padding-start: 0.5em;
+}
 li {
   list-style-type: none;
+}
+h2 {
+  color: gray;
+  font-family: sans-serif;
+  padding-left: 0.4em;
 }
 button {
   font-family: sans-serif;
   color: gray;
   background-color: transparent;
-  border-width: 0px;
+  border-width: 1px;
+  border-color: gray;
+  border-style: groove;
+  padding: 0.3em 0.5em;
+  margin-bottom: 5px;
   text-align: left;
   font-size: larger;
 }
